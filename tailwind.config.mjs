bc90242d21a1a98fac3ea698d943911eced3320d
Einar <einargudnig@gmail.com>
2024-06-19 12:48:45 +0000

    mdx images now working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+// import '@fontsource/geist-mono';
+// import '@fontsource/geist-sans';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			// fontFamily: {
+			// 	sans: ['Geist sans', ...defaultTheme.fontFamily.sans],
+			// },
+		},
 	},
 	plugins: [],
 }
